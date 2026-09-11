2976d6f51a8ad2656457dd9dd3a30d78c2c00e81
iMaro40 <maro.santiago@yahoo.com>
2026-09-11 21:22:36 +0800

    cache getProductById

--- backend/src/product/service.ts
+++ backend/src/product/service.ts
@@ -1,20 +1,35 @@
 import { database } from "../database";
+import { redisClient } from "../redis";
 import type { CreateProductInput } from "./dto/create-product";
 import type { Product } from "./model";
 import { ProductRepository } from "./repository";
+import type { RedisClientType } from "redis";
 
 export class ProductService {
-  public constructor(private readonly productRepository: ProductRepository) {}
+  public constructor(
+    private readonly productRepository: ProductRepository,
+    private readonly redis: RedisClientType,
+  ) {}
 
   public async createProduct(input: CreateProductInput): Promise<Product> {
     return this.productRepository.create(input);
   }
 
   public async getProductById(productId: string): Promise<Product | undefined> {
+    const cacheKey = `product:${productId}`;
+    const cachedProduct = await this.redis.get(cacheKey);
+
+    if (cachedProduct) {
+      return JSON.parse(cachedProduct) as Product;
+    }
+
     const product: Product | undefined =
       await this.productRepository.findById(productId);
 
-    // TO DO: Cache this maybe?
+    if (product) {
+      // Arbitrary 5 minute TTL for this assignment. Can be longer/shorter depending on production use case
+      await this.redis.set(cacheKey, JSON.stringify(product), { EX: 300 });
+    }
 
     return product;
   }
@@ -31,4 +46,5 @@ export class ProductService {
 
 export const productService: ProductService = new ProductService(
   new ProductRepository(database),
+  redisClient,
 );
